a92b94a890a87a3f7bc4884202465e54359684d8
gtvrofficial <w8361346@gmail.com>
2026-09-01 18:50:55 -0400

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.SITE_CONFIG = {
-  // Change this to your deployed rating API when you add it.
-  // Leave blank during beta to use local browser-only ratings.
-  RATING_API: "",
-  DATA_PATH: "./data"
-};
